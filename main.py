c59109721f769c74bdb5ddbb38dc2c9392ea585a
octavian96 <gogol.logog@hotmail.it>
2024-01-23 18:51:13 +0100

    Add test function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import os
 import telebot
+from telebot import types
 from src import utils as uts
 
 
@@ -41,6 +42,15 @@ if __name__ == "__main__":
         menu_recap = uts.recap(sender, menu_code)
         
         bot.reply_to(message, menu_recap)
+
+    @bot.message_handler(commands=['test_menu'])
+    def test_menu(message):
+        button = types.InlineKeyboardButton('Order your food!', web_app=types.WebAppInfo('https://96Octavian.github.io/menu_webapp/?code=test_menu'))
+
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.add(button)
+
+        bot.reply_to(message, text='Here is your link to today\'s menu', reply_markup=keyboard)
     
     bot.infinity_polling()
     
